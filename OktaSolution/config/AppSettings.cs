a6276d2303311a9c0b476946ea80e31ea77adc98
Your Name <antonius.vanhaeren@@telenet.be>
2020-07-02 22:10:30 +0200

    init app

---
+++ OktaSolution/config/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OktaSolution.config
+{
+    public class AppSettings
+    {
+        public string MongoConnectionString { get; set; }
+        public string OktaDomain { get; set; }
+        public string OktaClientId { get; set; }
+        public string OktaClientSecret { get; set; }
+    }
+}
